a8de52b4a5988eec93ea32e450dea729ea095f00
Kevin Tou <toukevin@gmail.com>
2019-08-16 10:17:14 -0700

    Created a form which adds a smurf to the BE

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,13 +1,44 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { addSmurf, getSmurfs } from '../actions';
 import './App.css';
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-      </div>
-    );
-  }
-}
 
-export default App;
+import FormikSmurfForm from './SmurfForm';
+
+const App = props => {
+  useEffect(() => {
+    if (props.smurfs.length === 0) {
+      props.getSmurfs();
+    }
+  }, [props.smurfs]);
+
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <FormikSmurfForm addSmurf={props.addSmurf} />
+      {props.smurfs.map(smurf => {
+        return (
+          <div key={smurf.id} className="smurf-card">
+            <p>Name: {smurf.name}</p>
+            <p>Age: {smurf.age}</p>
+            <p>Height: {smurf.height}</p>
+            <button>Remove</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    error: state.error,
+    isLoading: state.isLoading,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { addSmurf, getSmurfs },
+)(App);
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,7 +1,59 @@
-const initialState = {};
+import {
+  ADD_SMURF_START,
+  ADD_SMURF_SUCCESS,
+  ADD_SMURF_FAILURE,
+  GET_SMURFS_START,
+  GET_SMURFS_SUCCESS,
+  GET_SMURFS_FAILURE,
+} from '../actions';
+
+const initialState = {
+  smurfs: [],
+  error: '',
+  isLoading: false,
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_SMURF_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: '',
+      };
+    case ADD_SMURF_SUCCESS:
+      return {
+        ...state,
+        smurfs: [],
+        isLoading: false,
+        error: '',
+      };
+    case ADD_SMURF_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+      };
+    case GET_SMURFS_START:
+      return {
+        ...state,
+        isLoading: true,
+        error: '',
+      };
+    case GET_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        isLoading: false,
+        error: '',
+      };
+    case GET_SMURFS_FAILURE:
+      return {
+        ...state,
+        smurfs: [],
+        isLoading: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
