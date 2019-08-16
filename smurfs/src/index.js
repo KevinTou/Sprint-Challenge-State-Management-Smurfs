4400c1ca54bf99187f9fd61edb675d72951b1ef3
Kevin Tou <toukevin@gmail.com>
2019-08-16 08:30:45 -0700

    Fixed redux store imports

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'axios-progress-bar/dist/nprogress.css';
-import { createStore, applyMiddleWare } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './components/App';
 import reducer from './reducers';
 
-const store = createStore(reducer, applyMiddleWare(thunk));
+const store = createStore(reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
